7c4216634f8fdff5cd43b7756a6b5706152f5b2c
elmurzaev <elmurzaev@gmail.com>
2022-10-25 13:49:02 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+ //Выведение текста с помощью alert.
+ alert(777);
+ //Выведение в консоль.
+ console.log(11111);
+ //Верное объявление.
+ let age;
+ let info123;
+ let $size;
+ let _color;
+//Неверное объявление.
+let 123info;
+let my-age;
